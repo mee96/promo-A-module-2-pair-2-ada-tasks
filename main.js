3c8177316ce0008852c338f83381586e0a89332e
mee96 <carmenmc96@gmail.com>
2024-10-29 10:55:44 +0100

    reposi

---
+++ main.js
@@ -0,0 +1,13 @@
+
+
+
+const tasks = [
+  { name: "Recoger setas en el campo", completed: true, id: 1 },
+  { name: "Comprar pilas", completed: true, id: 2 },
+  { name: "Poner una lavadora de blancos", completed: true, id: 3 },
+  {
+    name: "Aprender cómo se realizan las peticiones al servidor en JavaScript",
+    completed: false,
+    id: 4,
+  },
+];
